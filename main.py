f0baa3e1e6b435d257304ad3bf349c1c378ee31c
Talha <muhammadtalha2003@yahoo.com>
2024-01-26 00:37:23 +0500

    add comments in main.py

--- main.py
+++ main.py
@@ -1,55 +1,67 @@
 import requests
 from bs4 import BeautifulSoup
+import pandas as pd
+import os 
+
+
+#------------------------
+csv_name = "BSAIF22.csv"
+#------------------------
+
+
+path_roll_numbers = os.path.join("source_csv", csv_name)
+roll_number = pd.read_csv(path_roll_numbers)
+roll_numbers = roll_number["roll_numbers"].values
 
 url = "https://portals.au.edu.pk/auresult/"
-search_reg_id = "220958"  # Replace with the actual registration ID
 
-# Define the headers
 headers = {
     "Host": "portals.au.edu.pk",
-    "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:121.0) Gecko/20100101 Firefox/121.0",
-    # ... (other headers)
+    "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:121.0) Gecko/20100101 Firefox/121.0"
 }
 
-# Define the payload data dynamically
-payload = {
-    "__EVENTTARGET": "",
-    "__EVENTARGUMENT": "",
-    "__VIEWSTATE": "",
-    "__VIEWSTATEGENERATOR": "",
-    "ctl00$AUContent$txt_regid": search_reg_id,
-    "__ASYNCPOST": "true",
-    "ctl00$AUContent$btnShow": "Search Result"
-}
+data_list = []
 
-response = requests.post(url, headers=headers, data=payload)
-print(response.status_code)
+for i, roll in enumerate(roll_numbers):
+    subjects = {}
+    print("\nFetching Result of: ", roll)
+    
+    subjects["name"] = roll_number.iloc[i]["names"]
+    subjects["roll_number"] = roll_number.iloc[i]["roll_numbers"]
+    
+    payload = {
+        "__EVENTTARGET": "",
+        "__EVENTARGUMENT": "",
+        "__VIEWSTATE": "",
+        "__VIEWSTATEGENERATOR": "",
+        "ctl00$AUContent$txt_regid": str(roll),
+        "__ASYNCPOST": "true",
+        "ctl00$AUContent$btnShow": "Search Result"
+    }
+    
+    response = requests.post(url, headers=headers, data=payload)
+    print(response.status_code)
+    
+    soup = BeautifulSoup(response.text, 'lxml')
+    # Find all tables on the page
+    tables = soup.find_all('table')
+    
 
-soup = BeautifulSoup(response.text, 'lxml')
+    # Iterate through all tables
+    if len(tables) >2:
+        for row in tables[2].find_all('tr')[2:]:
+            cols = row.find_all('td')
+            cols = [ele.text.strip() for ele in cols]
+            if len(cols)>1:
+                subjects[cols[0]] = cols[-1]
+            subjects["gpa"] = cols[0].split("\n")[-1]
+    else:
+        pass
 
-# Find all tables on the page
-tables = soup.find_all('table')
+    data_list.append(subjects)
+    # print(subjects)
+    
 
-# Initialize a set to store unique data
-unique_data = set()
+dataframe = pd.DataFrame(data_list)
+dataframe.to_csv(os.path.join("scrapped_result", csv_name.split(".")[0]+"result.csv"), index=False)
 
-# Iterate through all tables
-for table in tables:
-    # Iterate over each row in the table (skip the header row if necessary)
-    for row in table.find_all('tr'):
-        # Extract the text from each cell in the row
-        # and add it to a list representing that row
-        cols = row.find_all('td')
-        cols = [ele.text.strip() for ele in cols]
-        # Ensure that you have data (ignore empty/invalid rows)
-        if cols:
-            # Convert the list to a tuple to make it hashable
-            row_tuple = tuple(cols)
-            unique_data.add(row_tuple)
-
-# Convert the set of tuples back to a list of lists
-unique_data_list = [list(row_tuple) for row_tuple in unique_data]
-
-# Now 'unique_data_list' is a list of lists with unique entries
-for row in unique_data_list:
-    print(row)
